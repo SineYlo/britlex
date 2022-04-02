010aac634c8e0e343fc1d9cb5f58e491bf1bd210
SineYlo <56423781+SineYlo@users.noreply.github.com>
2022-04-02 12:31:59 +1000

    info: added another script file and a vector icon

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,5 @@ import './components/home/_preloader';
 import './components/home/_burger';
 // |=============== SCRIPT CONNECTION | SUPER PLACEHOLDER ===============>
 import './components/home/_placeholder';
+// |=============== SCRIPT CONNECTION | SCROLL SMOOTH ===============>
+import './components/home/_scroll-smooth';
